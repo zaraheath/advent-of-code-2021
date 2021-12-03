36b248698b685fe1d9911d9ca905857b2844b0c2
zara.heath <zara.heath@sequra.es>
2021-12-03 11:12:11 +0100

    moved into a common method

--- 03/calculate_life_support.rb
+++ 03/calculate_life_support.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class CalculateLifeSupport
   def self.run(**args)
     new(**args).run
@@ -14,31 +15,24 @@ class CalculateLifeSupport
       entries << line.strip.split("")
     end
     
-    oxygen_rating = entries.dup
-    oxygen_transposed = oxygen_rating.transpose
-    0.upto(oxygen_transposed.size - 1).each do |index|
-      counts = oxygen_transposed[index].tally
-      most_common = counts.select{|_, v| v == counts.values.max }.keys.first
-      less_common = counts.select{|_, v| v == counts.values.min }.keys.first
-      most_common = "1" if most_common == less_common
-      oxygen_rating = oxygen_rating.select {|e| e[index] == most_common }
-      break if oxygen_rating.size == 1
-      oxygen_transposed = oxygen_rating.transpose
-    end
+    get_value(entries.dup, :max, "1") * get_value(entries.dup, :min, "0")
+  end
 
-    co2_rating = entries.dup
-    co2_transposed = co2_rating.transpose
-    0.upto(co2_transposed.size - 1).each do |index|
-      byebug if co2_transposed[index].nil?
-      counts = co2_transposed[index].tally
-      most_common = counts.select{|_, v| v == counts.values.max }.keys.first
-      less_common = counts.select{|_, v| v == counts.values.min }.keys.first
-      less_common = "0" if most_common == less_common
-      co2_rating = co2_rating.select {|e| e[index] == less_common }
-      break if co2_rating.size == 1
-      co2_transposed = co2_rating.transpose
+private
+  def get_value(array, filter_method, equal_default)
+    transposed_array = array.transpose
+    0.upto(transposed_array.size - 1).each do |index|
+      counts = transposed_array[index].tally
+      searched_value = if counts.values.uniq.size == 1
+        equal_default
+      else
+        counts.select{|_, v| v == counts.values.send(filter_method)}.keys.first
+      end
+      array = array.select {|e| e[index] == searched_value }
+      break if array.size == 1
+      transposed_array = array.transpose
     end
-    oxygen_rating.first.join.to_i(2) * co2_rating.first.join.to_i(2)
+    array.first.join.to_i(2)
   end
 end
 
