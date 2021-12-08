d7d51ed576fe62633f6267e07ca7ab6572e85c51
zara.heath <zara.heath@sequra.es>
2021-12-08 19:15:38 +0100

    Day 8 Part 2

--- 08/digits_counter.rb
+++ 08/digits_counter.rb
@@ -7,17 +7,46 @@ class DigitsCounter
   end
 
   def run
-    segments = {
-      2 => 1,
-      3 => 7,
-      4 => 4,
-      7 => 8
-    }
+    segments = {}
     result = 0
     File.open(entries).each do |line|
       next if line.strip.empty?
-      sizes = line.strip.split(" | ").last.split(' ').map(&:size)
-      result = result + sizes.select{|n| segments.keys.include?(n)}.count
+      first = line.strip.split(" | ").first.split(' ')
+      segments["1"] = first.find{|i| i.size == 2}
+      first.delete(segments["1"])
+      segments["7"] = first.find{|i| i.size == 3}
+      first.delete(segments["7"])
+      segments["4"] = first.find{|i| i.size == 4}
+      first.delete(segments["4"])
+      segments["8"] = first.find{|i| i.size == 7}
+      first.delete(segments["8"])
+      segments["5"] = first.select do |item| 
+        item.size == 5 && (segments["4"].split('') - segments["1"].split('')).all?{|s| item.split('').include?(s) }
+      end.first
+      first.delete(segments["5"])
+      segments["3"] = first.select do |item| 
+        item.size == 5 &&  (item.split('') - segments["5"].split('')).size == 1
+      end.first
+      first.delete(segments["3"])
+      segments["2"] = first.select do |item| 
+        item.size == 5
+      end.first
+      first.delete(segments["2"])
+      segments["0"] = first.select do |item|
+        !item.split('').include?((segments["2"].split('') & segments["5"].split('') & segments["4"].split('')).first)
+      end.first
+      first.delete(segments["0"])
+      segments["9"]  = first.select do |item|
+        segments["1"].split('').all?{|n| item.split('').include?(n)}
+      end.first
+      first.delete(segments["9"])
+      segments["6"] = first.first
+
+      last = line.strip.split(" | ").last.split(' ')
+      digits = last.map do |digit|
+        segments.select{|k,v| v.split('').sort == digit.split('').sort }.keys.first
+      end
+      result = result + digits.join.to_i
     end
     result
   end
