a073fdaca3dd0074114e98eb4f307a6de87adc4b
zara.heath <zara.heath@sequra.es>
2021-12-08 16:29:07 +0100

    using hash

--- 08/digits_counter.rb
+++ 08/digits_counter.rb
@@ -7,12 +7,17 @@ class DigitsCounter
   end
 
   def run
-    target = [2,3,4,7]
+    segments = {
+      2 => 1,
+      3 => 7,
+      4 => 4,
+      7 => 8
+    }
     result = 0
     File.open(entries).each do |line|
       next if line.strip.empty?
       sizes = line.strip.split(" | ").last.split(' ').map(&:size)
-      result = result + sizes.select{|n| target.include?(n)}.count
+      result = result + sizes.select{|n| segments.keys.include?(n)}.count
     end
     result
   end
